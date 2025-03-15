8987eb262988c197592f66daa5680a0d697e3cc5
huncholane <huncho@hygo.ai>
2025-03-15 01:20:49 -0700

    'improved rust main'

--- 1_sorting_algorithms/countingsort.py
+++ 1_sorting_algorithms/countingsort.py
@@ -4,15 +4,22 @@ import time
 
 def countingsort(arr):
     # Initialize aux
-    k = max(arr)
+    l = arr[0]
+    r = arr[0]
+    for i in range(len(arr)):
+        if arr[i] < l:
+            l = arr[i]
+        elif arr[i] > r:
+            r = arr[i]
+    k = r - l
     aux = [0] * (k + 1)
     for num in arr:
-        aux[num] += 1
+        aux[num - l] += 1
     # Copy aux into arr
     i = 0
     for num in range(k + 1):
         while aux[num] > 0:
-            arr[i] = num
+            arr[i] = num - l
             i += 1
             aux[num] -= 1
 
--- 1_sorting_algorithms/src/main.rs
+++ 1_sorting_algorithms/src/main.rs
@@ -9,35 +9,64 @@ use strum::IntoEnumIterator;
 #[derive(Parser, Debug)]
 struct Args {
     /// How long is the array to sort?
+    /// Defaults to env DEFAULT_TEST_N or 50000 if algo is not present.
+    /// Defaults to env DEFAULT_DEBUG_N or 5 if algo is present.
     #[arg(short)]
     n: Option<usize>,
 
-    /// What is the range of random numbers?
+    /// What is the left of random numbers?
+    /// Defaults to env DEFAULT_LEFT or 0.
     #[arg(short)]
-    k: Option<i32>,
+    left: Option<i32>,
 
-    /// Select specific algo
+    /// What is the right of the random numbers?
+    /// Defaults to env DEFAULT_RIGHT or n.
+    #[arg(short)]
+    right: Option<i32>,
+
+    /// Want to debug an algo?
+    /// Debugs specific algo if present.
+    /// Tests all algos enabled with env if not present.
     #[arg(short)]
     algo: Option<SortingAlgo>,
 }
 
 fn main() {
     let args = Args::parse();
-    let n = match args.n {
+    let n: usize = match args.n {
         Some(n) => n,
-        _ => 50000,
+        _ => match args.algo.is_some() {
+            true => match option_env!("DEFAULT_DEBUG_N") {
+                Some(n) => n.parse().expect("Expected integer for DEFAULT_DEBUG_N"),
+                _ => 5,
+            },
+            _ => match option_env!("DEFAULT_TEST_N") {
+                Some(n) => n.parse().expect("Expected integer for DEFAULT_TEST_N"),
+                _ => 50000,
+            },
+        },
     };
-    let k = match args.k {
-        Some(k) => k,
-        _ => n as i32,
+    let l: i32 = match args.left {
+        Some(l) => l,
+        _ => match option_env!("DEFAULT_LEFT") {
+            Some(l) => l.parse().expect("Expected an integer for DEFAULT_LEFT"),
+            _ => 0,
+        },
+    };
+    let r: i32 = match args.right {
+        Some(r) => r,
+        _ => match option_env!("DEFAULT_RIGHT") {
+            Some(r) => r.parse().expect("Expected an integer for DEFAULT_RIGHT"),
+            _ => n as i32,
+        },
     };
     let pb = ProgressBar::new_spinner();
-    let style = ProgressStyle::with_template("{spinner} Loading Random numbers").unwrap();
+    let style = ProgressStyle::with_template("{spinner} Loading Random Numbers").unwrap();
     pb.set_style(style);
     pb.enable_steady_tick(Duration::from_millis(100));
-    let mut arr: Vec<i32> = (0..n).map(|_| rand::random_range(0..k)).collect();
+    let mut arr: Vec<i32> = (0..n).map(|_| rand::random_range(l..r)).collect();
     pb.finish_and_clear();
-    println!("n={}, k={}", n, k);
+    println!("n={}, l={}, r={}", n, l, r);
     if let Some(algo) = args.algo {
         println!("Debugging {}", algo);
         algo.debug(&mut arr);
