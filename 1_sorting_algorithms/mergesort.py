378ae7b83fadaad05a38b5db0d7a054fea5be543
huncholane <huncho@hygo.ai>
2025-03-17 11:25:59 -0700

    'ok im bad'

--- 1_sorting_algorithms/mergesort.py
+++ 1_sorting_algorithms/mergesort.py
@@ -2,37 +2,42 @@ import random
 import time
 
 
-def helper(arr, l, r):
-    print(l, r)
-    if l == r:
+def mergesort_helper(arr, start, end):
+    # Leaf node
+    if start == end:
         return
-    mid = (r - l) // 2
-    helper(arr, l, mid)
-    helper(arr, mid + 1, r)
+    # Internal node worker
+    mid = int((start + end) / 2)
+    mergesort_helper(arr, start, mid)
+    mergesort_helper(arr, mid + 1, end)
+    i, j = start, mid + 1
     aux = []
-    i, j = l, mid + 1
-    while i <= mid and j <= r:
-        if arr[i] <= arr[j]:
-            aux.append(arr[i])
-            i += 1
-        else:
+    while i <= mid and j <= end:
+        if arr[j] <= arr[i]:
             aux.append(arr[j])
             j += 1
+        else:
+            aux.append(arr[i])
+            i += 1
+    # Gather phase
     while i <= mid:
         aux.append(arr[i])
         i += 1
-    while j <= mid:
+    while j <= end:
         aux.append(arr[j])
         j += 1
-    arr[l:r] = aux[:]
+    # Copy phase
+    for i, num in enumerate(aux):
+        arr[start + i] = num
 
 
 def mergesort(arr):
-    helper(arr, 0, len(arr) - 1)
+    """O(nlogn) very fast but requires extra memory"""
+    mergesort_helper(arr, 0, len(arr) - 1)
 
 
 if __name__ == "__main__":
-    n = 5
+    n = 50000000
     arr = [random.randint(0, n) for i in range(n)]
 
     start = time.time()
