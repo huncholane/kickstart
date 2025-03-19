dc792cd9d96816ddf2737039b6b57dced5065837
huncholane <huncho@hygo.ai>
2025-03-19 09:46:02 -0700

    'dutch select is perfect'

--- 2_sorting_algorithms_class/dutchselect.py
+++ 2_sorting_algorithms_class/dutchselect.py
@@ -18,6 +18,8 @@ def partition(arr, l, r):
 
 
 def helper(arr, l, r, target):
+    if l >= r:
+        return arr[l]
     pl, pr = partition(arr, l, r)
     if pl <= target <= pr:
         return arr[target]
@@ -29,11 +31,13 @@ def helper(arr, l, r, target):
 
 def dutchselect(arr, k):
     n = len(arr)
+    if k > n:
+        return
     return helper(arr, 0, n - 1, n - k)
 
 
 n = 10
-k = 2
+k = 11
 arr = [random.randint(0, n) for _ in range(n)]
 print(arr)
 answer = dutchselect(arr, k)
