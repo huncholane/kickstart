889bea8a2a4f3667ac38938cee29852dace4c0ac
huncholane <huncho@hygo.ai>
2025-04-08 00:32:01 -0700

    'understand cross edge'

--- 5_graphs/preclass/bfs_traversal.py
+++ 5_graphs/preclass/bfs_traversal.py
@@ -6,20 +6,34 @@ def bfs_traversal(n, edges):
     visited = [False] * n
     parent = [None] * n
     captured = []
+    cc = []
+    cross_edges = []
     for i in range(n):
         if not visited[i]:
+            cc.append([])
             q = [i]
             visited[i] = True
             while q:
                 v = q.pop(0)
                 captured.append(v)
+                cc[-1].append(v)
                 for w in adj_list[v]:
                     if not visited[w]:
                         parent[w] = v
                         q.append(w)
                         visited[w] = True
-    return captured
+                    elif parent[v] != w and parent[v] is not None:
+                        cross_edges.append([w, v])
+    return captured, cc, cross_edges
 
 
-captured = bfs_traversal(**{"n": 6, "edges": [[0, 1], [0, 2], [0, 4], [2, 3]]})
-print(captured)
+captured, connected_captures, cross_edges = bfs_traversal(
+    **{"n": 6, "edges": [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]}
+)
+print("All Captured", captured)
+print("Connected Captures")
+for cc in connected_captures:
+    print(cc)
+print("Cross Edges")
+for ce in cross_edges:
+    print(ce)
