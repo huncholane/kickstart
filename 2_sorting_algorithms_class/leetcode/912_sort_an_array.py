c88ef3410aa0f4ec11302d0a0fe924fb9e665b6e
huncholane <huncho@hygo.ai>
2025-04-09 10:45:53 -0700

    'added sort an array solution'

---
+++ 2_sorting_algorithms_class/leetcode/912_sort_an_array.py
@@ -0,0 +1,34 @@
+import random
+from typing import List
+
+# The description is asking for nlogn with smallest space complexity
+# This points to quicksort
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def part3(l, r):
+            """Must use three way partition to combat repeated numbers."""
+            pivot = nums[random.randint(l, r)]
+            i, j, k = l, l, r
+            while j <= k:
+                if nums[j] < pivot:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                    j += 1
+                elif nums[j] == pivot:
+                    j += 1
+                else:  # nums[j]>pivot
+                    nums[j], nums[k] = nums[k], nums[j]
+                    k -= 1
+            return i, k
+
+        def helper(l, r):
+            if l >= r:
+                return
+            pl, pr = part3(l, r)
+            helper(l, pl - 1)
+            helper(pr + 1, r)
+
+        helper(0, len(nums) - 1)
+        return nums
