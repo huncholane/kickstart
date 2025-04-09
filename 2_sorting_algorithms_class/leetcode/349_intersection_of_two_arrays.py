594cb7fad42b1085fa11dc0f032116ed7816a925
huncholane <huncho@hygo.ai>
2025-04-09 10:50:26 -0700

    'add solution for 349 intersection of two arrays'

---
+++ 2_sorting_algorithms_class/leetcode/349_intersection_of_two_arrays.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    """The question asks for unique and any order.
+
+    - Presort to make faster.
+    - Use set since order does not matter."""
+
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
+        res = set()
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] == nums2[j]:
+                res.add(nums1[i])
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return list(res)
