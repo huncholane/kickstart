0919cc3e9a2ff42e0534c75eb3e5d6a9ebfad946
huncholane <huncho@hygo.ai>
2025-05-15 18:35:16 -0700

    made solution for 139 cleaner

--- 5_dynamic_programming/leetcode/139_wordbreak/solution1.py
+++ 5_dynamic_programming/leetcode/139_wordbreak/solution1.py
@@ -1,16 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
         n = len(s)
-        word_set = {w for w in wordDict}
-        # call table f to force myself to think of this as a f(n)
-        f = [False] * (n + 1)
-        f[0] = True
+        table = [False] * (n + 1)
+        table[0] = True
         for i in range(1, n + 1):
-            for j in range(1, i):
-                # Go backwords to improve speed
-                if f[i - j] and s[i - j : i] in word_set:
-                    f[i] = True
+            for j in range(i, -1, -1):
+                if table[j] and s[j:i] in wordDict:
+                    table[i] = True
                     break
-            if s[:i] in word_set:
-                f[i] = True
-        return f[n]
+        return table[n]
